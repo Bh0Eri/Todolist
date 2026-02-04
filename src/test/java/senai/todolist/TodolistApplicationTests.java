50d4d18ccc2858379433d1b70a462d9da2478e59
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-02-04 10:08:40 -0300

    chore: updated java version & refactor api layers

---
+++ src/test/java/senai/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
