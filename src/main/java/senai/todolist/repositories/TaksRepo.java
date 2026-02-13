71011560f5e85d9d18792f510267bcaf170c250d
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-02-13 11:18:39 -0300

    fix: changed long to UUID

--- src/main/java/senai/todolist/repositories/TaksRepo.java
+++ src/main/java/senai/todolist/repositories/TaksRepo.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import senai.todolist.entity.Tasks;
 
+import java.util.UUID;
+
 @Repository
-public interface TaksRepo extends JpaRepository<Tasks, Long> {
+public interface TaksRepo extends JpaRepository<Tasks, UUID> {
 
     Tasks findTasksByTaskName(String taskName);
 
--- src/main/java/senai/todolist/repositories/UserRepo.java
+++ src/main/java/senai/todolist/repositories/UserRepo.java
@@ -2,12 +2,14 @@ package senai.todolist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import senai.todolist.dto.userDTO.UserResponseDTO;
 import senai.todolist.entity.Users;
 
+import java.util.UUID;
 
 
 @Repository
-public interface UserRepo extends JpaRepository<Users, Long> {
+public interface UserRepo extends JpaRepository<Users, UUID> {
 
     Users findUsersByEmail(String email);
 
