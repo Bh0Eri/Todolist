6e37d7e653b8b86dbc5b9596a3267cd28806fa87
Bh0Eri <ericmoreiradosreis@gmail.com>
2026-02-22 02:24:48 -0300

    feat: completed task and user controller

--- src/main/java/senai/todolist/controller/TaskController.java
+++ src/main/java/senai/todolist/controller/TaskController.java
@@ -1,7 +1,54 @@
 package senai.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.*;
+import senai.todolist.dto.taskDTO.TaskRequestDTO;
+import senai.todolist.dto.taskDTO.TaskResponseDTO;
+import senai.todolist.entity.Tasks;
+import senai.todolist.service.taskService.TaskService;
+import senai.todolist.service.taskService.TaskServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("todo/")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final TaskService taskService;
+
+
+    @PostMapping(value = "create")
+    public TaskRequestDTO createTask(@RequestBody TaskRequestDTO task) {
+        taskService.create(task);
+        return task;
+    }
+
+    @GetMapping(value = "show")
+    public Optional<Tasks> showTask(@RequestParam UUID id) {
+        return taskService.showOne(id);
+    }
+
+    @GetMapping(value = "showAll")
+    public List<TaskResponseDTO> showAllTasks() {
+        return taskService.showAll();
+    }
+
+    @DeleteMapping(value = "delete")
+    public void deleteTask(@RequestParam UUID id) {
+        taskService.delete(id);
+    }
+
+    @PostMapping(value = "update")
+    public TaskRequestDTO updateTask(@RequestBody UUID id, TaskRequestDTO task) {
+        if (taskService.showOne(id).isPresent()) {
+            taskService.update(id, task);
+            return task;
+        }
+        return null;
+    }
+
 }
--- src/main/java/senai/todolist/controller/UserController.java
+++ src/main/java/senai/todolist/controller/UserController.java
@@ -1,9 +1,52 @@
 package senai.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import senai.todolist.dto.userDTO.UserRequestDTO;
+import senai.todolist.dto.userDTO.UserResponseDTO;
+import senai.todolist.entity.Users;
+import senai.todolist.service.userService.UsersService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("user/")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UsersService usersService;
+
+    @PostMapping(value = "create")
+    public String save(@RequestBody UserRequestDTO user) {
+        usersService.create(user);
+        return "success";
+    }
+
+    @GetMapping(value = "showAll")
+    public List<UserResponseDTO> findAll() {
+        return usersService.findAll();
+    }
+    @GetMapping(value = "show")
+    public Optional<Users> findById(@RequestBody UUID id) {
+        return usersService.findById(id);
+    }
+    @DeleteMapping(value = "delete")
+    public String delete(@RequestBody UUID id) {
+        usersService.delete(id);
+        return "success";
+    }
+
+    @PostMapping(value = "update")
+    public String update(@RequestBody UUID id, UserRequestDTO user) {
+        if (usersService.findById(id).isPresent()) {
+            usersService.update(id, user);
+            return "success";
+        }else  {
+            return "error";
+        }
+    }
 
 
 
